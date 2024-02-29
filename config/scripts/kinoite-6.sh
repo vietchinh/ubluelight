95dfa765fc2f6b4badfec14b7858a7d81e0e5216
vietchinh <1348151+vietchinh@users.noreply.github.com>
2024-02-29 07:18:04 +0100

    feat: fedora 40
    Signed-off-by: vietchinh <1348151+vietchinh@users.noreply.github.com>

---
+++ config/scripts/kinoite-6.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+# Tell this script to exit if there are any errors.
+# You should have this in every custom script, to ensure that your completed
+# builds actually ran successfully without any errors!
+set -oue pipefail
+
+# Your code goes here.
+systemctl enable rpm-ostreed-automatic.timer
+#systemctl enable flatpak-system-update.timer
+
+#systemctl --global enable flatpak-user-update.timer
+
+#cp /usr/share/ublue-os/update-services/etc/rpm-ostreed.conf /etc/rpm-ostreed.conf
+
+ln -s "/usr/share/fonts/google-noto-sans-cjk-fonts" "/usr/share/fonts/noto-cjk"
